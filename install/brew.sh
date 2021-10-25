d930c4e7fbdfa195c1012a58800ab86b5838ec7d
Adam <aophof46@users.noreply.github.com>
2021-10-25 17:57:09 -0400

    Create brew.sh

---
+++ install/brew.sh
@@ -0,0 +1,32 @@
+# Install Homebrew
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$(whoami)/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+    
+    
+    
+brew update
+brew upgrade
+
+# Install packages
+
+apps=(
+    aerial
+    arduino
+    iterm2
+    sublime-text2
+    vmware-horizon-client
+    android-studio
+    balenaetcher
+    powershell
+    visual-studio-code
+    dockutil
+    ffmpeg
+)
+
+brew install "${apps[@]}"
+
+# Git comes with diff-highlight, but isn't in the PATH
+ln -sf "$(brew --prefix)/share/git-core/contrib/diff-highlight/diff-highlight" /usr/local/bin/diff-highlight
