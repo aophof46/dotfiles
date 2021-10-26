f73967192c92b7c50c8e846c8987dd19b305db05
Adam <aophof46@users.noreply.github.com>
2021-10-26 18:00:38 -0400

    Update brew-cask.sh

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -1,11 +1,21 @@
+# Install Homebrew
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 
 if [ "$(echo $SHELL)" == "/bin/zsh" ]; then
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$(whoami)/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi   
 
+
+brew update
+brew upgrade
+
 # Install packages
 apps=(
+    git
+    screen
     aerial
     arduino
     iterm2
@@ -22,4 +32,4 @@ apps=(
 brew install "${apps[@]}" --cask
 
 # Git comes with diff-highlight, but isn't in the PATH
-# sudo ln -sf "$(brew --prefix)/share/git-core/contrib/diff-highlight/diff-highlight" /usr/local/bin/diff-highlight
+sudo ln -sf "$(brew --prefix)/share/git-core/contrib/diff-highlight/diff-highlight" /usr/local/bin/diff-highlight
