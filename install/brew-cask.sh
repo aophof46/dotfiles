4c2e4ffcd7ee57bfcf03e2df35d7d3d467b5b8d2
aophof46 <aophof46@gmail.com>
2022-03-09 16:28:41 -0500

    commented out the  brew cask install command since there are no casks to install

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -51,7 +51,7 @@ casks=(
 
 brew install "${apps[@]}"
 
-brew install "${casks[@]}" --casks
+#brew install "${casks[@]}" --casks
 
 # Git comes with diff-highlight, but isn't in the PATH
 sudo ln -sf "$(brew --prefix)/share/git-core/contrib/diff-highlight/diff-highlight" /usr/local/bin/diff-highlight
