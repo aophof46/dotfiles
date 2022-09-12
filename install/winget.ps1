8064dbe1dc933913d6a85b237469f5e5efa489f1
Ophoff <Adam.Ophoff@meijer.com>
2022-09-12 11:08:40 -0400

    added domain join section with rsat install

--- install/winget.ps1
+++ install/winget.ps1
@@ -1,3 +1,26 @@
+function Test-RegistryValue {
+    param (
+
+     [parameter(Mandatory=$true)]
+     [ValidateNotNullOrEmpty()]$Path,
+
+    [parameter(Mandatory=$true)]
+     [ValidateNotNullOrEmpty()]$Value
+    )
+
+    try {
+        Get-ItemProperty -Path $Path | Select-Object -ExpandProperty $Value -ErrorAction Stop | Out-Null
+        return $true
+    }
+    catch {
+        return $false
+    }
+}
+
+
+
+
+
 $apps = @(
 "7zip.7zip",
 "balena.etcher",
@@ -20,5 +43,39 @@ foreach($app in $apps) {
 
 write-host "Installing: $app"
 & winget install $app
+
+}
+
+
+# Check if computer part of domain
+[bool]$DomainConnected = [bool](Get-WmiObject -Class Win32_ComputerSystem).PartOfDomain
+
+# Check if computer is on workgroup
+[bool]$WorkgroupConnected = [bool](Get-WmiObject -Class Win32_ComputerSystem).Workgroup
+
+if($DomainConnected -and !$WorkgroupConnected) {
+    $DomainName = $env:USERDNSDOMAIN
+
+    write-host "Connected to $DomainName" -BackgroundColor Green -ForegroundColor Black
+
+
+    # Check reg key value to avoid WSUS error
+    $registryPath = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\Servicing"
+    $registryValueName = "RepairContentServerSource"
+    $registryValueData = '2'
+    $registryType = "DWORD"
+
+    if(Test-RegistryValue -Path $registryPath -Value $registryValueName) {
+        write-host "Setting registry value"
+        Set-ItemProperty -Path $registryPath -Name $registryValueName -Value $registryValueData -Force
+    }
+    else {
+        write-host "creating registry value"
+        New-ItemProperty -Path $registryPath -Name $registryValueName -Value $registryValueData -PropertyType $registryType -Force
+    }
+
+    write-host "Install RSAT" -BackgroundColor Green -ForegroundColor Black
+    Get-WindowsCapability -Name RSAT* -Online | Add-WindowsCapability -Online
+
 
 }
